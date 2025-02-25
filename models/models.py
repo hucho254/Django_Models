aceac6b064040699232ed26c218bfa6ada8a36d3
hucho254 <billybillions28@gmail.com>
2025-02-25 09:29:04 +0300

    newly added files

--- models/models.py
+++ models/models.py
@@ -7,6 +7,8 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     author = models.CharField(max_length=100)
+    email = models.EmailField(null=True, blank=True)
+    phone_number = models.CharField(max_length=11, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
